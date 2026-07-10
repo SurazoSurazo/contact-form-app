f112a9ffd313a26efcf6af5cd1ba772163acfa4a
DAICHI IWAMA <5eM33dcd4r0M3WXWqNn@gmail.com>
2026-07-10 09:45:51 +0900

    feat: シーダーを実装

---
+++ database/seeders/TagSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Tag;
+use Illuminate\Database\Seeder;
+
+class TagSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $tags = [
+            '質問',
+            '要望',
+            '不具合報告',
+            'ご意見',
+            'その他',
+        ];
+
+        foreach ($tags as $name) {
+            Tag::updateOrCreate(['name' => $name]);
+        }
+    }
+}
