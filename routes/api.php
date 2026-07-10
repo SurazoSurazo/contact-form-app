ccb1305d21ab3f0267fc8790f9feea886a14b8a7
DAICHI IWAMA <5eM33dcd4r0M3WXWqNn@gmail.com>
2026-07-10 09:50:06 +0900

    feat: ルーティングを実装

---
+++ routes/api.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Http\Controllers\Api\V1\ContactController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::get('/v1/contacts', [ContactController::class, 'index']);
+Route::get('/v1/contacts/{contact}', [ContactController::class, 'show']);
+Route::post('/v1/contacts', [ContactController::class, 'store']);
+Route::put('/v1/contacts/{contact}', [ContactController::class, 'update']);
+Route::delete('/v1/contacts/{contact}', [ContactController::class, 'destroy']);
